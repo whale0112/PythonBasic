a91b1b404dada15853d6a43aa73a465c7fde0367
whale0112 <91520694+whale0112@users.noreply.github.com>
2021-09-29 15:53:11 +0900

    Create main1.py

---
+++ 2강/main1.py
@@ -0,0 +1,64 @@
+print("안녕하세요! 오늘은 '수요일' 입니다")
+print("월요일, 화요일, 수요일, 목요일, 금요일")
+print("월요일","화요일","수요일","목요일","금요일",sep=", ")
+a=10000+25000+30000
+print(a)
+print(a/3)
+print("총 금액 :", a, "평균 금액 :", a/3)
+print("""컴퓨터는 재밌고, 파이썬도 재밌다.""")
+print('''컴퓨터는 재밌고, 파이썬도 재밌다.''')
+print("\"파이썬은 쉽다\"라고 '철수'가 말했어요.")
+print('"파이썬은 쉽다"라고 \'철수\'가 말했어요.')
+print("\"파이썬은 쉽다\"라고 \'철수\'가 말했어요.")
+print("프로그래밍 언어는 다양하다.\n파이썬은 프로그래밍 언어이다.")
+print('''프로그래밍 언어는 다양하다.
+파이썬은 프로그래밍 언어이다.''')
+print("""프로그래밍 언어는 다양하다.
+파이썬은 프로그래밍 언어이다.""")
+string = '''프로그래밍 언어는 다양하다.
+파이썬은 프로그래밍 언어이다.'''
+print(string)
+string = """프로그래밍 언어는 다양하다.
+파이썬은 프로그래밍 언어이다."""
+print(string)
+print('''오늘 날씨는 비가 오고
+나는 오늘 파이썬을 배우러 왔다.''')
+head = "파이썬은 "
+tail = "재미있다"
+print(head + tail)
+head = "오늘 날씨는"
+tail =  " 비가 온다"
+print(head + tail)
+print("head" + "tail")
+a = "파이썬"
+print(a * 2)
+print("=" * 30)
+print("My Program")
+print("=" * 30)
+a = "여의도역은 여기에서 가깝다.5255"
+print(len(a))
+string = "오늘 날씨는 비가 오고\n나는 오늘 파이썬을 배우러 왔다"
+print(string)
+print(len(string))
+a = "Life is too short, You need Python"
+print(a[-5])
+print(a[0] + a[1] + a[2] + a[3])
+print(a[28] + a[29] + a[30] + a[31] + a[32] + a[33])
+print(a[-34] + a[-33] + a[-32] +a[-31])
+print(a[-6] + a[-5] + a[-4] + a[-3] + a[-2] + a[-1])
+print(a[0:4])
+print(a[5:7])
+print(a[8:11])
+print(a[12:18])
+print(a[19:22])
+print(a[23:27])
+print(a[28:34])
+print(a[19:])
+print(a[:17])
+print(a[:])
+print(a[19:-7])
+a = "20210929Rainy"
+date = a[:8]
+weather = a[8:]
+print(date)
+print(weather)
