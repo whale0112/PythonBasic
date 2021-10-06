137c4f18ed3587b46f4dfc7bc079a3f9d165b58d
whale0112 <91520694+whale0112@users.noreply.github.com>
2021-10-06 15:50:02 +0900

    Create main1.py

---
+++ 3강/main1.py
@@ -0,0 +1,36 @@
+age = 13
+print(age)
+공책 = 3
+필통 = 1
+교과서 = 2
+print(공책, 필통, 교과서)
+age = 13
+age = 13
+print(age)
+string = '''오늘은 비가 오고
+나는 오늘 졸업사진을 찍었다'''
+print(string)
+print('''\    /\\
+ )  ( ')
+(  /  )
+ \(__)|''')
+print('''|\_/|
+|q p|   /}
+( 0 )"""\\
+|"^"`    |
+||_/=\___|''')
+head = "오늘 날씨는 "
+tail = "비가 온다"
+print(head + tail)
+print("=" * 50)
+print("My Program")
+print("=" * 50)
+A = 1
+B = 2
+print(A+B)
+print(A-B)
+print(A*B)
+print(A/B)
+print(A%B)
+hello = "안녕하세요! 오늘은 기분이 좋습니다."
+print(hello[5])
