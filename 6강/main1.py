3b24d0bbce1943d4a047d0ae6673ada42b7baebe
whale0112 <91520694+whale0112@users.noreply.github.com>
2021-10-27 15:58:37 +0900

    Create main1.py

---
+++ 6강/main1.py
@@ -0,0 +1,109 @@
+if 3<5:
+    print("조건식이 True이므로 실행됩니다.")
+if 3>5:
+    print("조건식이 False이므로 실행되지 않습니다.")
+apple = 10
+if apple >= 5:
+    print("사과를 나누어 먹는다")
+else:
+    print("걸어 가라")
+apple = 3
+if apple >= 5:
+    print("사과를 나누어 먹는다")
+else:
+    print("사과를 혼자 먹는다")
+money = 10000
+if money >= 3000:
+    print("택시를 타고 가라")
+else:
+    print("걸어 가라")
+#rank = input('몇 등인가요(1 또는 2)?')
+#if rank == '1':
+    print('TV를 보며 편안하게 쉬세요')
+#else:
+    print('설거지 당첨!')
+number = 15
+if number % 3 == 0:
+    print("{}는 3의 배수입니다.".format(number))
+number = 16
+if number % 3 == 0:
+    print("{}는 3의 배수입니다.".format(number))
+print(3 > 5)
+print(3 < 5)
+print(3 == 5)
+print(3 != 5)
+x = 4
+print(1 < x < 5)
+if 3<5:
+    print("3은 5보다 작다.")
+if 3>5:
+    print("3은 5보다 크다.")
+if 4 < 3:
+    print("Hello World.")
+else:
+    print("Hi, there")
+if True :
+    print("1")
+    print("2")
+else :
+    print("3")
+    print("4")
+#if grade >= 2 and grade <= 4:
+    #print("햄버거를 주세요")
+#else:
+    #print("김밥을 주세요")
+print((3 == 3) and (4 != 3))
+#data = input("입력값: ")
+# data = 2
+# if int(data) % 2 == 0:
+#     print("짝수")
+# else:
+#     print("홀수")
+#data = input("입력값: ")
+#if int(data) + 20 > 255:
+   #print(255)
+#else:
+    #print(int(data) + 20)
+#pizza = input('피자 가게가 열렸나요(예/아니요)?')
+#chicken = input('치킨 가게가 열렸나요(예/아니요)?')
+#중국집 = input('중국집이 열렸나요(예/아니요)?')
+#if pizza == '예':
+    #print('피자 가게로 가자')
+#elif chicken == '예':
+    #print('치킨 가게로 가자')
+#elif 중국집 == '예':
+    #print("중국집으로 가자")
+#else:
+    #print('편의점에서 라면을 먹자')
+#data = input("입력값: ")
+#if int(data) - 20 < 0:
+    #print(0)
+#else:
+    #if int(data) - 20 > 255:
+        #print(255)
+    #else:
+        #print(int(data) - 20)
+#score = input("점수: ")
+#if int(score) >= 81 and int(score) <= 100:
+    #print("A등급")
+#elif int(score) >= 61 and int(score) <= 80:
+    #print("B등급")
+#elif int(score) >= 41 and int(score) <= 60:
+    #print("C등급")
+#elif int(score) >= 21 and int(score) <= 40:
+    #print("D등급")
+#elif int(score) >= 0 and int(score) <= 20:
+    #print("E등급")
+#else:
+    #print("오류")
+num1 = input("input number1: ")
+num2 = input("input number2: ")
+num3 = input("input number3: ")
+if int(num1) > int(num2) and int(num1) > int(num3):
+    print(num1)
+elif int(num2) > int(num3) and int(num2) > int(num3):
+    print(num2)
+elif int(num3) > int(num1) and int(num3) > int(num2):
+    print(num3)
+else:
+    print("오류")        
