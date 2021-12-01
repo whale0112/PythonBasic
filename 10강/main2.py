644722b8a370cec90079ffd54fb7c981a4ac18f4
whale0112 <91520694+whale0112@users.noreply.github.com>
2021-12-01 15:48:55 +0900

    Create main2.py

---
+++ 10강/main2.py
@@ -0,0 +1,30 @@
+movie_rank = ['아바타', '어벤져스', '타이타닉', '명량', '겨울왕국']
+movie_rank.append('겨울왕국2')
+movie_rank = ['아바타', '어벤져스', '타이타닉', '명량', '겨울왕국2']
+movie_rank.insert(4,'겨울왕국')
+movie_rank = ['아바타', '어벤져스', '타이타닉', '명량', '겨울왕국2']
+del movie_rank[1]
+movie_rank = ['아바타', '어벤져스', '타이타닉', '명량', '겨울왕국2']
+movie_rank.remove('어벤져스')
+lang1 = ["C", "C++", "JAVA"]
+lang2 = ["Python", "Go", "C#"]
+langs = lang1 + lang2
+my_variable = ()
+movie_rank = ('아바타', '어벤져스', '타이타닉', '명량', '겨울왕국')
+t = (1, 2, 3)
+#t[0] = 'a'
+#t = ('주먹', '가위', '보')
+#data = int(input('1, 2, 3 중 하나를 입력해주세요. '))
+#if data >= 1 and data <= 3:
+    #print(t[data - 1])
+#else:
+    #print('오류')
+#t = ('주먹', '가위', '보')
+#while True:
+    #data = int(input('1, 2, 3 중 하나를 입력해주세요. '))
+    #if data >= 1 and data <= 3:
+        #print(t[data - 1])
+    #elif data == -1:
+        #break
+    #else:
+        #print('오류')
